4823bb9312b24851a9dcaefdfd41fceb934dd84f
Sasha <mazurova.alexandra@gmail.com>
2021-03-01 18:17:07 +0300

    connecting to vk

--- callback_handler.py
+++ callback_handler.py
@@ -19,7 +19,7 @@ async def processing(request):
     data = await request.json()
     #Вконтакте в своих запросах всегда отправляет поле типа
     if 'type' not in data.keys():
-        return 'not vk'
+        return web.Response(text='not vk')
     if data['type'] == 'confirmation' and data['group_id'] == 202927298:
         return web.Response(text=confirmation_token)
     elif data['type'] == 'message_new':
@@ -29,4 +29,4 @@ async def processing(request):
         message = data['object']['body']
         api.messages.send(access_token=token, user_id=str(user_id), message=message, random_id=random.getrandbits(64))
         # Сообщение о том, что обработка прошла успешно
-        return 'ok'
+        return web.Response(text='ok')
