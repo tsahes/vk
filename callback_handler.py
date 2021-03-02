1220c4e54166fa4f04f75410350b22c5e8c0c913
Sasha <mazurova.alexandra@gmail.com>
2021-03-02 15:05:58 +0300

    fix vk connection

--- callback_handler.py
+++ callback_handler.py
@@ -17,22 +17,22 @@ token = os.environ.get('vk_api_token')
 async def processing(request):
     #Распаковываем json из пришедшего POST-запроса
     data = await request.json()
-    print(data['type'])
-    print(data)
     #Вконтакте в своих запросах всегда отправляет поле типа
     if 'type' not in data.keys():
         return web.Response(text='not vk')
+
     if data['type'] == 'confirmation' and data['group_id'] == 202927298:
         return web.Response(text=confirmation_token)
+
     elif data['type'] == 'message_new':
         session = vk.Session()
         api = vk.API(session, v='5.130')
         conversations = api.messages.getConversations(access_token=token,)
-        print(data['object'].keys())
-        if 'from_id' in data['object']:
-            user_id = data['object']['from_id']
-        else:
-            user_id = ''
+        message = data['object']['message']
+        peer_id = message['peer_id']
+        text = message['text']
+        user_id = message['from_id']
+
 #        message = data['object']['text']
 #        print(message)
         group_id = data['group_id']
@@ -41,9 +41,9 @@ async def processing(request):
             chat_id = data['object']['chat_id']
         else:
             chat_id = ''
-        api.messages.send(access_token=token, user_id='3577708',
-                          peer_id='3577708', group_id=group_id,
-                          chat_id='3577708', message=str(data),
+        api.messages.send(access_token=token, user_id=str(user_id),
+                          peer_id=str(peer_id), group_id=group_id,
+                          message=str(data),
                           random_id=random.getrandbits(64))
         # Сообщение о том, что обработка прошла успешно
         return web.Response(text='ok')
