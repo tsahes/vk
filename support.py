e2e615c9718499638b6738fae1a6f12fed331c2a
Sasha <mazurova.alexandra@gmail.com>
2021-03-05 16:42:38 +0300

    fixing bugs with setting themes

--- support.py
+++ support.py
@@ -67,7 +67,7 @@ async def get_played_themes(collection, group_id):
                               'game_finished': True}).distinct('theme')
 #                             {'theme': True})
     api.messages.send(peer_id=group_id, random_id=random.getrandbits(64),
-                      message=str([r async for r in result]))
+                      message=str(result))
 #    return [r async for r in result]
     return result
 
