34c4ca5e4cc6c9aaea11cd5a0f26f07ff6e8b7b2
Sasha <mazurova.alexandra@gmail.com>
2021-03-07 21:21:02 +0300

    implementing time checker

--- game_functions.py
+++ game_functions.py
@@ -4,7 +4,7 @@ from support import get_question_order, insert_document, get_themes, get_played_
 import time
 
 
-seconds_to_answer_question = 15
+seconds_to_answer_question = 5
 
 
 async def form_correct_question(question_data):
