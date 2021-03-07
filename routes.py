df4602b489170bd6efc824eeb56b673b8027777e
Sasha <mazurova.alexandra@gmail.com>
2021-03-07 19:34:24 +0300

    route fix

--- routes.py
+++ routes.py
@@ -8,7 +8,7 @@ from controllers import (questions_list, questions_create, questions_delete,
 def setup_routes(app):
     app.add_routes([web.get('/', empty),
                     web.get('/api/questions/list', questions_list),
-                    web.get('api/games/list', games_list),
+                    web.get('/api/games/list', games_list),
                     web.post('/api/questions/create', questions_create),
                     web.post('/api/questions/delete', questions_delete),
                     web.post('/vk', processing),
