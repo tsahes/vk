f0181d62621860656762f7ddbc3a654310d0d182
Sasha <mazurova.alexandra@gmail.com>
2021-03-15 17:39:30 +0300

    tests for correct behavior of question api

--- tests/test_questions.py
+++ tests/test_questions.py
@@ -3,11 +3,9 @@ import unittest
 import aiounittest
 import json
 
-from connection_to_database import questions_table
-from support import get_question_order, gen_question_id
+from support import gen_question_id
 
 SUCCESS = 200
-INCORRECT_HEADER = 400
 
 
 class TestQuestions(aiounittest.AsyncTestCase):
@@ -15,18 +13,16 @@ class TestQuestions(aiounittest.AsyncTestCase):
     def __init__(self, *a, **kw):
         super(TestQuestions, self).__init__(*a, **kw)
         self.host = 'tsahes-svoya-igra.herokuapp.com/api/questions'
-        self.commands = ['list', 'create', 'delete']
+        self.commands = ['create', 'list', 'delete']
         self.urls = {command: 'http://{}/{}'.format(self.host, command) for command in self.commands}
+        self.question_counter = 1
 
     async def test_data_creation(self):
-        # random_name = "{} {}".format(choice(FIRST_NAMES), choice(LAST_NAMES))
-        # position = "{} {}".format(choice(SENIORITY), choice(POSITIONS))
-        # kwargs = {'name': random_name, 'position': position}
         test_question = {'points': 0, 'theme': 'test_theme', 'text': 'test_text',
                          'answers': {'text': 'test_answer', 'id': 0, 'is_correct': True, 'order': 0}}
 
         # order = await get_question_order(questions_table, test_question)
-        order = 1
+        order = self.question_counter
         question_id = gen_question_id(test_question['theme'], order)
 
         status_code, text = self._create_question(**test_question)
@@ -37,40 +33,55 @@ class TestQuestions(aiounittest.AsyncTestCase):
         self.assertEqual(status_code, SUCCESS)
         self.assertEqual(text['data'], test_question)
 
-    # def test_data_retrieve(self):
-    #     status_code, accounts = self._get_accounts()
-    #     self.assertEqual(status_code, SUCCESS)
-    #     self.assertGreater(len(accounts.get('candidates')), 1)
-    #
-    #     status_code, account = self._get_accounts(identificator=1)
-    #     self.assertEqual(status_code, SUCCESS)
-    #     self.assertTrue(isinstance(account.get('candidate'), dict))
-    #
-    # def test_delete(self):
-    #     identificator = choice(self._get_list_of('id'))
-    #     status_code, text = self._delete_account(identificator)
-    #     self.assertEqual(status_code, SUCCESS)
-    #     self.assertNotIn(identificator, self._get_list_of('id'))
-    #
-    # def _get_accounts(self, identificator=None):
-    #     _url = self.url
-    #     if identificator:
-    #         _url = "{}/{}".format(self.url, identificator)
-    #     _response = requests.get(_url)
-    #     return _response.status_code, _response.json()
-    #
-    # def _get_list_of(self, key):
-    #     _, data = self._get_accounts()
-    #     return map(lambda x: x.get(key), data.get('candidates'))
-    #
+    def test_data_retrieve(self):
+        status_code, questions = self._get_questions()
+        self.assertEqual(status_code, SUCCESS)
+        self.assertGreaterEqual(questions.get('data').get('total'), 1)
+
+        params = {'limit': 1, 'offset': None, 'theme': None}
+        status_code, questions = self._get_questions(**params)
+        self.assertEqual(status_code, SUCCESS)
+        self.assertEqual(questions.get('data').get('total'), 1)
+
+        params['limit'] = None
+        params['offset'] = 1
+        status_code, questions = self._get_questions(**params)
+        self.assertEqual(status_code, SUCCESS)
+        self.assertEqual(questions.get('data').get('total'), self.question_counter - 1)
+
+        params['offset'] = None
+        params['theme'] = 'test_theme'
+        status_code, questions = self._get_questions(**params)
+        self.assertEqual(status_code, SUCCESS)
+        self.assertIsInstance(questions.get('data').get('questions'), list)
+
+    def test_delete(self):
+        identifier = 'test_theme1'
+        status_code, text = self._delete_question(identifier)
+        self.assertEqual(status_code, SUCCESS)
+        self.assertNotIn(identifier, self._get_list_of('id'))
+
+    def _get_questions(self, limit=None, offset=None, theme=None):
+        _payload = {'limit': limit, 'offset': offset, 'theme': theme}
+        _response = requests.get(self.urls['list'], params=_payload)
+        return _response.status_code, _response.json()
+
+    def _get_list_of(self, key):
+        _, total_response = self._get_questions()
+        data = total_response['data']
+        return map(lambda x: x.get(key), data.get('questions'))
+
     def _create_question(self, points, theme, text, answers):
         _payload = json.dumps({'points': points, 'theme': theme, 'text': text, 'answers': answers})
         _response = requests.post(self.urls['create'], data=_payload)
+        self.question_counter += 1
         return _response.status_code, _response.json()
 
-    # def _delete_account(self, identificator):
-    #     _response = requests.delete("{}/{}".format(self.url, identificator))
-    #     return _response.status_code, _response.json()
+    def _delete_question(self, identifier):
+        _payload = json.dumps({'id': identifier})
+        _response = requests.post(self.urls['delete'], data=_payload)
+        print(_response)
+        return _response.status_code, _response.json()
 
 
 if __name__ == '__main__':
