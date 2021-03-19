e4243a6746c956ba535a738dc2361d35b7e5d024
Sasha <mazurova.alexandra@gmail.com>
2021-03-19 19:07:53 +0300

    env variable fix

--- connection_to_database.py
+++ connection_to_database.py
@@ -6,10 +6,9 @@ client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URL)
 db = client.vk_game
 
 test = os.environ.get('TEST')
-print(test)
-if test == 0:
+if test == '0':
     questions_table = db.questions
     games_table = db.games
-else:
+elif test == '1':
     questions_table = db.test_questions
     games_table = db.test_games
